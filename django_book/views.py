57ac32adeaa2cd478f18778cbfffd46c8a002e58
LMelo <luis.nickelback@gmail.com>
2014-04-01 22:33:31 -0300

    Chapter 4: Templates

--- django_book/views.py
+++ django_book/views.py
@@ -3,6 +3,9 @@
 
 from datetime import datetime, timedelta
 from django.http import HttpResponse, Http404
+from django.template import Template, Context
+from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def hello(request):
@@ -11,8 +14,13 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.now()
-    html = "<html><body>It is now %s</body></html>" % now
-    return HttpResponse(html)
+    # Cap 4. (1º)
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date': now}))
+    #return HttpResponse(html)
+
+    # Cap 4. (2º)
+    return render(request, 'current_datetime.html', {'current_date': now})
 
 
 def hours_ahead(request, offset):
@@ -21,5 +29,9 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.now() + timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    # Cap 4. (1º)
+    # html = "<html><body>In %s hour(s), it will be %s</body></html>" % (offset, dt)
+    # return HttpResponse(html)
+
+    # Cap 4. (2º)
+    return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
