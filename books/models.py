c6afceb176276ad83951e5feea349e358bbf9689
LMelo <luis.nickelback@gmail.com>
2014-04-30 22:54:59 -0300

    Chapter 10: Advanced Models

--- books/models.py
+++ books/models.py
@@ -25,11 +25,24 @@ class Author(models.Model):
         return u'%s %s' % (self.first_name, self.last_name)
 
 
+class DahlBookManager(models.Manager):
+    def get_queryset(self):
+        return super(DahlBookManager, self).get_queryset().filter(author='Roald Dahl')
+
+
+class BookManager(models.Manager):
+    def title_count(self, keyword):
+        return self.filter(title__icontains=keyword).count()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField(blank=True, null=True)
+    num_pages = models.IntegerField(blank=True, null=True)
+    objects = BookManager()
+    dahl_objects = DahlBookManager()
 
     def __unicode__(self):
         return self.title
